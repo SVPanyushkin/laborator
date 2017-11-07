075823f8c92f57f215e613cb004d78dfc4cfaa15
SVPanyushkin <svpanyushkin@gmail.com>
2017-11-07 11:08:24 +0300

    07112017

--- lab6.php
+++ lab6.php
@@ -1,20 +1,28 @@
 <?php
-$myword = 'oflw';
+/*Панюшкин Сергей
+Лабораторная работа №6
+Аннаграммами будем считать слова у которых кол-во и состав букв в слове одинкаовые,
+т.е. если отсортировать буквы слова по алфавиту, то у анаграмм эти строки будут совпадать
+fowl -- flow
+wolf -- flow
+*/
 
-    $mymassiv = str_split($myword);
+$myword = 'oflw';  //то что будем искать
+
+    $mymassiv = str_split($myword);  //преобразуем в массив, чтобыпотом отсортировать
     sort($mymassiv, SORT_STRING);
-    $sortmyword=implode($mymassiv);
-    $file = file('words.txt');
+    $sortmyword=implode($mymassiv);  //обратно в строку
+    $file = file('words.txt'); //открываем файл. содержмое в массив
 
-    for ($i=0; $i<count($file); $i++)
+    for ($i=0; $i<count($file); $i++)  //циклом проходимся по по каждому слову, сортируя буквы слова по алфавиту
     {
         $word = $file[$i];
         $nword=trim($word);
         $massiv = (str_split($nword));
         sort($massiv, SORT_STRING);
         $sortword=implode($massiv);
-        if ($sortmyword === $sortword)
+        if ($sortmyword === $sortword)  // сравниваем с исходным словом $myword
         {
-            print_r($word);
+            print_r($word); //выводим найденные анаграммы
         }
     }
